974d1d45dbb7ca4c5288a262f285a137011636da
pecolos <pecolos103@gmail.com>
2024-12-11 07:17:09 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,10 @@
+let num = 15;
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+console.log('3と5の倍数');
+} else if (num % 3 === 0) {
+  console.log('3の倍数');
+} else if (num % 5 === 0); {
+  console.log('５の倍数');
+}
